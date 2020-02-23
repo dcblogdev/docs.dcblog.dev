c87854be83bdda0e5fa6478e35b936e8a94b31ad
David Carr <dave@dcblog.dev>
2020-02-23 01:22:22 +0000

    added to menu

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -45,7 +45,8 @@
 								Packages Docs
 							</a>
 							<div class="dropdown-menu dropdown-menu-right rounded shadow menu-animate slideIn" aria-labelledby="navbarDropdown">
-								<a class="dropdown-item has-icon" href="/laravel-dropbox"><span class="theme-icon-holder mr-2"><i class="fas fa-map-signs fa-fw"></i></span>Laravel Dropbox API</a>
+								<a class="dropdown-item has-icon" href="/laravel-box"><span class="theme-icon-holder mr-2"><i class="fas fa-map-signs fa-fw"></i></span>Laravel Box API</a>
+                                <a class="dropdown-item has-icon" href="/laravel-dropbox"><span class="theme-icon-holder mr-2"><i class="fas fa-map-signs fa-fw"></i></span>Laravel Dropbox API</a>
 								<a class="dropdown-item has-icon" href="/laravel-microsoft-graph"><span class="theme-icon-holder mr-2"><i class="fas fa-arrow-down fa-fw"></i></span>Laravel Microsoft Graph API</a>
 								
 							</div>
